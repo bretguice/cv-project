57886fe684c7d29a8bbb772537aaea83d7c52fe6
bret <bret.guice@gmail.com>
2022-03-16 12:37:54 -0500

    cleaned up initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  
 }
 
 export default App;
+
+/* 
+Create Resume Builder App
+Components:
+  - Personal Info
+  - Education
+  - Work Experience
+
+Personal Info
+All created by input fields
+  - First Name
+  - Last Name
+  - Phone
+  - email
+
+Education 
+create objects with props
+need submit button to create object
+  - school
+  - city
+  - year graduated
+  - degree
+
+Work Epxerience
+create objects with props
+need submit buttont o create object
+  - employer
+  - start date
+  - end date
+  - job title
+  - description
+
+
+Input screen and preview screen
+how can I save as a PDF?
+data storage
+
+
+
+
+*/
